baba587c800e494e52cd5da5239afa5a2508d0c1
adilbilal-maker <adilbilal453@gmail.com>
2026-01-08 19:19:43 +0500

    Time: 9 ms (34.02%) | Memory: 13.5 MB (7.41%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> roman = {{'I', 1}, {'V', 5}, {'X',10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+
+        int integer = 0;
+        int n = s.length();
+
+        for(int i = 0; i<n ; i++){
+            if(i<n-1 && roman[s[i]]<roman[s[i+1]]){
+                integer = integer - roman[s[i]];
+            }
+            else{
+                integer = integer + roman[s[i]];
+            }
+        }
+        return integer;
+    }
+};
