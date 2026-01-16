bd6076d16bea29887c912a11ac187afcddf05b34
adilbilal-maker <adilbilal453@gmail.com>
2026-01-16 21:29:11 +0500

    Time: 0 ms (100.00%) | Memory: 11.4 MB (75.88%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int lastIndex = digits.size()-1;
+        for(int i = lastIndex; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
